7e0deb7ad57ac29d2732e8e4d99bb58483279756
Mash2 <mashjosephk01@gmail.com>
2022-10-29 11:10:20 +0300

    Modified js file

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
-document.getElementsByClassName('Services');
-document.getElementById("demo").addEventListener("mouseover", mouseOver);
-document.getElementById("demo").addEventListener("mouseout", mouseOut);
-
-function mouseOver() {
-  document.getElementById("demo").style.color = "red";
-}
-
-function mouseOut() {
-  document.getElementById("demo").style.color = "black";
-}
+$(document).ready(function(){
+    $("form#contact-form").submit(function(event){
+      // event.preventDefault();
+      var name = $("input#name").val();
+      var email = $("input#email").val();
+      var subject=$("input#subject").val();
+      var message = $("textarea#message").val();
+      if ($("input#name").val() && $("input#email").val()){
+        alert (name + ", we have received your message. Thank you for reaching out to us.");
+      }
+      else {
+        alert("Please enter your name and email!");

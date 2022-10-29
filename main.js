6437bf68cdfac7c922bd54213a35119d26a38ad1
Mash2 <mashjosephk01@gmail.com>
2022-10-29 12:50:04 +0300

    mouse events added

--- main.js
+++ main.js
@@ -1,12 +1,10 @@
-$(document).ready(function(){
-    $("form#contact-form").submit(function(event){
-      // event.preventDefault();
-      var name = $("input#name").val();
-      var email = $("input#email").val();
-      var subject=$("input#subject").val();
-      var message = $("textarea#message").val();
-      if ($("input#name").val() && $("input#email").val()){
-        alert (name + ", we have received your message. Thank you for reaching out to us.");
-      }
-      else {
-        alert("Please enter your name and email!");
+document.getElementById("service").onmouseover = function() {mouseOver()};
+document.getElementById("service").onmouseout = function() {mouseOut()};
+
+function mouseOver() {
+  document.getElementById("service").style.color = "green";
+}
+
+function mouseOut() {
+  document.getElementById("service").style.color = "red";
+}

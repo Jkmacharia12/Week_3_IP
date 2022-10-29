41e1f0d50f86f5b10d1468c88d0e29f797b36932
Mash2 <mashjosephk01@gmail.com>
2022-10-29 14:52:35 +0300

    fuction modified

--- main.js
+++ main.js
@@ -39,21 +39,5 @@ document.getElementById("square").onclick = function() {
     document.getElementById("square").style.display = "none";
 
 }
-$(document).ready(function(){
-    $("form#contact-form").submit(function(event){
-      // event.preventDefault();
-      var name = $("input#name").val();
-      var email = $("input#email").val();
-      var subject=$("input#subject").val();
-      var message = $("textarea#message").val();
-      if ($("input#name").val() && $("input#email").val()){
-        alert (name + ", we have received your message. Thank you for reaching out to us.");
-      }
-      else {
-        alert("Please enter your name and email!");
-      }
-      
-    });
-  
-  });
-
+var nameFilled=document.document.getElementByName(name);
+alert(name);

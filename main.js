b8b59f715a11f03f381c3e5b1f1c7dc15621bd5d
Mash2 <mashjosephk01@gmail.com>
2022-10-29 14:34:31 +0300

    new form added

--- main.js
+++ main.js
@@ -40,9 +40,20 @@ document.getElementById("square").onclick = function() {
 
 }
 $(document).ready(function(){
-    $("#products").hover(function(){
-      $(this).css("background-color", "yellow");
-      }, function(){
-      $(this).css("background-color", "pink");
+    $("form#contact-form").submit(function(event){
+      // event.preventDefault();
+      var name = $("input#name").val();
+      var email = $("input#email").val();
+      var subject=$("input#subject").val();
+      var message = $("textarea#message").val();
+      if ($("input#name").val() && $("input#email").val()){
+        alert (name + ", we have received your message. Thank you for reaching out to us.");
+      }
+      else {
+        alert("Please enter your name and email!");
+      }
+      
     });
+  
   });
+

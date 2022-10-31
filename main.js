1bf2055efa9b358d3950dcc3af43bee9fc901633
Mash2 <mashjosephk01@gmail.com>
2022-10-31 09:07:23 +0300

    Jquery fuction added

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+$(document).ready(function(){
+    $("#productImage").click(function(){
+      $("#content").slideDown("slow");
+    });
+  });
+
 document.getElementById("service").onmouseover = function() {mouseOver()};
 document.getElementById("service").onmouseout = function() {mouseOut()};
 
@@ -8,31 +14,3 @@ function mouseOver() {
 function mouseOut() {
   document.getElementById("service").style.color = "red";
 }
-document.getElementById("products").onclick = function() {
-  
-    document.getElementById("products").slide="flex";
-}
-
-document.getElementById("products").onclick = function() {
-  
-    document.getElementById("products").style.display="flex";
-
-}
-document.getElementById("products").onclick = function() {
-  
-    document.getElementById("products").style.display="flex";
-
-}
-document.getElementById("products").onclick = function() {
-  
-    document.getElementById("products").style.display="flex";
-
-}
-document.getElementByClass("AboutUs").onclick = function() {
-
-    document.getElementById("AboutUs").style.color = "blue";
-
-}
-
-var nameFilled=document.document.getElementByName(name);
-alert(name);

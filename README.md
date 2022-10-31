a3da19298762a49e30599bea804d539608e12e84
Mash2 <mashjosephk01@gmail.com>
2022-10-31 10:48:48 +0300

    README added

---
+++ README.md
@@ -0,0 +1,17 @@
+# {Application Name}
+#### Mash softwares site.
+#### By Joseph Macharia
+## Its a site giving customers more information about Mashsofwares Limited.
+
+## This is an interactive site and Fun using
+* learning more of what the company does.
+
+## Known Bugs
+Submiiting details
+## Technologies Used
+CSS,HTML,BOOTSTRAP Jquery and JAVA scipt
+## Support and contact details
+mashjosephk01@gmail.com
+### License
+*Joseph Macharia Kamau*
+Copyright (c) {year} **{List of contributors or company name}**
--- main.js
+++ main.js
@@ -1,32 +1,29 @@
-$(document).ready(function(){
-    $("#productImage").click(function(){
-      $("#content").slideToggle("slow");
-    });
-
-    });
-    $(document).ready(function(){
-        $("#productImage1").click(function(){
-          $("#content1").slideToggle("slow");
-        });
-    
-        });
-        $(document).ready(function(){
-            $("#productImage2").click(function(){
-              $("#content2").slideToggle("slow");
-            });
-        
-            });
-            $(document).ready(function(){
-                $("#productImage3").click(function(){
-                  $("#content3").slideToggle("slow");
-                });
-            
-                });
-
-
-
-document.getElementById("service").onmouseover = function() {mouseOver()};
-document.getElementById("service").onmouseout = function() {mouseOut()};
+$(document).ready(function () {
+  $("#productImage").click(function () {
+    $("#content").slideToggle("slow");
+  });
+});
+$(document).ready(function () {
+  $("#productImage1").click(function () {
+    $("#content1").slideToggle("slow");
+  });
+});
+$(document).ready(function () {
+  $("#productImage2").click(function () {
+    $("#content2").slideToggle("slow");
+  });
+});
+$(document).ready(function () {
+  $("#productImage3").click(function () {
+    $("#content3").slideToggle("slow");
+  });
+});
+document.getElementById("service").onmouseover = function () {
+  mouseOver();
+};
+document.getElementById("service").onmouseout = function () {
+  mouseOut();
+};
 
 function mouseOver() {
   document.getElementById("service").style.color = "green";

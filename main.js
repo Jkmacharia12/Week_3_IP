9bc70946a8ad9931e98ea7f973e5e26e1c12c379
Mash2 <mashjosephk01@gmail.com>
2022-10-31 09:36:04 +0300

    added Jquery fuctions

--- main.js
+++ main.js
@@ -4,8 +4,24 @@ $(document).ready(function(){
     });
 
     });
-  
-
+    $(document).ready(function(){
+        $("#productImage1").click(function(){
+          $("#content1").slideToggle("slow");
+        });
+    
+        });
+        $(document).ready(function(){
+            $("#productImage2").click(function(){
+              $("#content2").slideToggle("slow");
+            });
+        
+            });
+            $(document).ready(function(){
+                $("#productImage3").click(function(){
+                  $("#content3").slideToggle("slow");
+                });
+            
+                });
 
 
 
